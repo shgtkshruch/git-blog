ba614494d375b1c14893d7194fbe19db449c70a3
shgtkshruch <shgtk.shruch@gmail.com>
2014-11-23 09:04:34 +0900

    Add test case for multiple files.

--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
-var assert = require('assert');
 var fs = require('fs');
+var assert = require('assert');
 var getDiff = require('../lib/git/diff.js');
 var createFiles = require('../lib/git/createFiles');
 var rimraf = require('rimraf');
@@ -8,7 +8,7 @@ describe('git-blog', function () {
   var diff;
 
   before(function (done) {
-    var data = fs.readFileSync('./test/stub.txt', {encoding: 'utf-8'});
+    var data = fs.readFileSync('./test/stub.txt', 'utf8');
     getDiff(data, function (_diff) {
       diff = _diff;
       done();
@@ -16,31 +16,100 @@ describe('git-blog', function () {
   });
 
   describe('diff', function () {
-    describe('get diff from git data', function () {
-      it('should have expected file path', function () {
-        diff.forEach(function (file) {
-          assert.deepEqual(file.path, 'app/styles/style.scss');
-        });
-      });
 
-      it('should have expected diff address', function () {
-        diff.forEach(function (file) {
-          file.chunks.forEach(function (chunk) {
+    describe('get diff from git data', function () {
+
+      describe('scss', function () {
+        var scss;
+
+        before(function () {
+          scss = diff[0];
+        });
+
+        it('should have expected file path', function () {
+          assert.deepEqual(scss.path, 'app/styles/style.scss');
+        });
+
+        describe('chunk former', function () {
+          var chunk;
+
+          before(function () {
+            chunk = scss.chunks[0];
+          });
+
+          it('should have expected diff address', function () {
             assert.deepEqual(chunk.num, '-1,3 +1,4');
+          });
+
+          it('should have expected diff body', function () {
+            assert.deepEqual(chunk.body, ' p {\n   color: red;\n+  font-size: 16px;\n }\n');
+          });
+        });
+
+        describe('chunk latter', function () {
+          var chunk;
+
+          before(function () {
+            chunk = scss.chunks[1];
+          });
+
+          it('should have expected diff address', function () {
+            assert.deepEqual(chunk.num, '-11,3 +11,4');
+          });
+
+          it('should have expected diff body', function () {
+            assert.deepEqual(chunk.body, ' p {\n   color: red;\n-  font-size: 16px;\n }\n');
           });
         });
       });
 
-      it('should have expected diff body', function () {
-        diff.forEach(function (file) {
-          file.chunks.forEach(function (chunk) {
-            assert.deepEqual(chunk.body, ' p {\n   color: red;\n+  font-size: 16px;\n }\n');
+      describe('html', function () {
+        var html;
+
+        before(function () {
+          html = diff[1];
+        });
+
+        it('should have expected file path', function () {
+          assert.deepEqual(html.path, 'app/index.html');
+        });
+
+        describe('chunk former', function () {
+          var chunk;
+
+          before(function () {
+            chunk = html.chunks[0];
+          });
+
+          it('should have expected diff address', function () {
+            assert.deepEqual(chunk.num, '-1,1 +1,2');
+          });
+
+          it('should have expected diff body', function () {
+            assert.deepEqual(chunk.body, ' <p>Hello world</p>\n+ <p>Hello world</p>\n');
+          });
+        });
+
+        describe('chunk latter', function () {
+          var chunk;
+
+          before(function () {
+            chunk = html.chunks[1];
+          });
+
+          it('should have expected diff address', function () {
+            assert.deepEqual(chunk.num, '-10,1 +10,2');
+          });
+
+          it('should have expected diff body', function () {
+            assert.deepEqual(chunk.body, ' <h1>Hello world</h1>\n- <h2>Hello world</h2>\n');
           });
         });
       });
     });
 
     describe('create files from diff data', function () {
+
       before(function (done) {
         diff.forEach(function (file) {
           createFiles(file);
